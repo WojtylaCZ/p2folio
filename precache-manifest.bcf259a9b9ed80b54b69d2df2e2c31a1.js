7d1e1feb20148f2a6f15cdf8a61e0192c043693d
Vojtech Uhlir <uhlirvoj1@gmail.com>
2019-09-20 18:39:54 +0200

    Updates

---
+++ precache-manifest.bcf259a9b9ed80b54b69d2df2e2c31a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bb6010e3b9cf1309bb20c2abb7a1b3",
+    "url": "/p2folio/index.html"
+  },
+  {
+    "revision": "3d731ef4f6db40818e4b",
+    "url": "/p2folio/static/css/main.cc535bf0.chunk.css"
+  },
+  {
+    "revision": "94958452e27433bf6235",
+    "url": "/p2folio/static/js/2.507cde55.chunk.js"
+  },
+  {
+    "revision": "3d731ef4f6db40818e4b",
+    "url": "/p2folio/static/js/main.98ed98bf.chunk.js"
+  },
+  {
+    "revision": "fe585872547c640270c4",
+    "url": "/p2folio/static/js/runtime-main.e9836360.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/p2folio/static/media/logo.5d5d9eef.svg"
+  }
+]);
