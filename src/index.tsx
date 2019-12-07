0ee26acf25126199ab5d4547582dbd0493e05652
Vojtech Uhlir <uhlirvoj1@gmail.com>
2019-12-07 09:11:22 +0100

    Added react-ga solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './components/App';
+
+ReactGA.initialize('UA-149846559-1');
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(<App />, document.getElementById('root'));
