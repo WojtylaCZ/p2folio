cc55044c66682402189e9aa8f12ff2b65f62510c
Vojtech Uhlir <uhlirvoj1@gmail.com>
2020-01-10 13:21:50 +0100

    Updates

---
+++ precache-manifest.63f5e744037d2646dadd4bca8481c83e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a2d5170293b10a0b2f24336c5d2394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d5fcc7f0df251609c2",
+    "url": "/static/css/main.3fd156b5.chunk.css"
+  },
+  {
+    "revision": "ce92a15eb22df944de55",
+    "url": "/static/js/2.1148c221.chunk.js"
+  },
+  {
+    "revision": "26d5fcc7f0df251609c2",
+    "url": "/static/js/main.5ee54803.chunk.js"
+  },
+  {
+    "revision": "6185cda505be10dc5c3a",
+    "url": "/static/js/runtime-main.0f6f1acc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a1f25d47527ff59aab1011fdce0e1c95.js"
+  "/precache-manifest.63f5e744037d2646dadd4bca8481c83e.js"
 );
 
 self.addEventListener('message', (event) => {
