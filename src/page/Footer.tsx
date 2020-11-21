9a2e06fa58981570aec203140346b6e098d9d574
Vojtech Uhlir <uhlirvoj1@gmail.com>
2020-11-21 21:20:56 +0100

    Added github icon

--- src/page/Footer.tsx
+++ src/page/Footer.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { SocialIcon } from '../shared/components/SocialIcon';
 import { FacebookIcon } from '../shared/icons/components/FacebookIcon';
+import { GitHubIcon } from '../shared/icons/components/GitHub';
 import { TelegramIcon } from '../shared/icons/components/TelegramIcon';
 import { TwitterIcon } from '../shared/icons/components/TwitterIcon';
 
 export const Footer = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginTop: '2em' }}>
+      <a href="https://github.com/WojtylaCZ/p2folio" target="_blank">
+        <SocialIcon style={{ margin: '0.5em' }} icon={<GitHubIcon />} />{' '}
+      </a>
       <SocialIcon style={{ margin: '0.5em' }} icon={<FacebookIcon />} />
       <SocialIcon style={{ margin: '0.5em' }} icon={<TwitterIcon />} />
       <SocialIcon style={{ margin: '0.5em' }} icon={<TelegramIcon />} />
